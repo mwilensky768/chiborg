65521f2bac836832d0b7809487f1b54fc03371b5
Mike Wilensky <wilensky.michael@gmail.com>
2025-03-27 16:09:48 -0400

    refer to data_container attribute instead of jk_data

--- chiborg/jk_hyp.py
+++ chiborg/jk_hyp.py
@@ -11,35 +11,39 @@ import warnings
 tm_prior = namedtuple("tm_prior", ["func", "bounds", "params", "name"])
 multi_gauss_prior = namedtuple("multi_gauss_prior", ["mean", "cov"])
 
-class jk_hyp():
+class HypContainer:
 
-    def __init__(self, jk_data, bias_mean, bias_cov,
+    def __init__(self, data_container, bias_mean, bias_cov,
                  tmp=tm_prior(norm.pdf, [-np.inf, np.inf],
                               {"loc": 0, "scale": 0}, "gaussian"),
                  hyp_prior=None, mode="diagonal"):
         """
         Args:
-            jk_data: A jk_data object containing the data for the hypothesis
-                test.
-            bias_mean: If mode is 'diagonal' or 'partition', must use a vector.
+            data_container: 
+                A DataContainer or SimContainer object 
+            bias_mean: 
+                If mode is 'diagonal' or 'partition', must use a vector.
                 The values represent the mean parameter of the bias prior for
                 any hypothesis in which that bias is active (detailed in the
                 paper). If mode is 'manual', then this must be an array with
                 first dimension of length equal to number of hypotheses
                 considered, and second dimension equal to the number of data.
-            bias_cov: Covariances for the bias priors of the hypotheses in
+            bias_cov: 
+                Covariances for the bias priors of the hypotheses in
                 consideration. If mode is 'diagonal' or 'partition', must use a
                 vector. The values represent the variances of the priors. If
                 mode is 'manual', then must supply a sequence of covariance
                 matrices for each hypothesis in consideration.
-            tmp: tm_prior namedtuple containing a function to be evaluated for
-                 numerical marginalization over the true mean prior as well as
-                 the integration bounds and any parameters that the prior has.
-                 It also has a name attribute that describes the type of prior.
-                 For proper interaction with the jk_calc class in analytic mode,
-                 a Gaussian prior must be used with the name 'Gaussian', as in
-                 the default argument case.
-            mode: Which set of hypotheses to use. Valid options are 'diagonal',
+            tmp: 
+                tm_prior namedtuple containing a function to be evaluated for
+                numerical marginalization over the true mean prior as well as
+                the integration bounds and any parameters that the prior has.
+                It also has a name attribute that describes the type of prior.
+                For proper interaction with the jk_calc class in analytic mode,
+                a Gaussian prior must be used with the name 'Gaussian', as in
+                the default argument case.
+            mode: 
+                Which set of hypotheses to use. Valid options are 'diagonal',
                 'partition', and 'manual'. The first two are detailed in the
                 paper, and are essentially summaries of the set of bias
                 covariance matrices in consideration. The third indicates that
@@ -50,7 +54,7 @@ class jk_hyp():
         if mode not in valid_modes:
             raise ValueError(f"mode keyword must be one of {valid_modes}")
         self.mode = mode
-        self.jk_data = copy.deepcopy(jk_data)
+        self.data_container = copy.deepcopy(data_container)
         self.num_hyp = self.get_num_hyp(bias_cov)
 
         if self.mode != "manual":
@@ -85,7 +89,7 @@ class jk_hyp():
                 to set num_hyp in manual mode.
         """
         if self.mode == 'partition': # Calculate the (N + 1)th Bell Number
-            M = self.jk_data.num_dat + 1
+            M = self.data_container.num_dat + 1
             B = np.zeros(M + 1, dtype=int)
             B[0] = 1  # NEED THE SEED
             for n in range(M):
@@ -93,7 +97,7 @@ class jk_hyp():
                     B[n + 1] += comb(n, k, exact=True) * B[k]
             num_hyp = B[M]
         elif self.mode == 'diagonal':
-            num_hyp = 2**(self.jk_data.num_dat)
+            num_hyp = 2**(self.data_container.num_dat)
         else:
             num_hyp = len(bias_cov)
         return(num_hyp)
@@ -114,15 +118,15 @@ class jk_hyp():
             bias_cov_final: Array of covariance matrices for the bias priors.
                 One for each hypothesis.
         """
-        bias_cov_shape = [self.num_hyp, self.jk_data.num_dat, self.jk_data.num_dat]
+        bias_cov_shape = [self.num_hyp, self.data_container.num_dat, self.data_container.num_dat]
         bias_cov_final = np.zeros(bias_cov_shape)
-        bias_mean_final = np.zeros([self.num_hyp, self.jk_data.num_dat])
+        bias_mean_final = np.zeros([self.num_hyp, self.data_container.num_dat])
 
         ###
         # The matrices need to be transitive - this should be all of them. #
         ###
         hyp_ind = 0
-        for diag_on in powerset(range(self.jk_data.num_dat)):
+        for diag_on in powerset(range(self.data_container.num_dat)):
             N_on = len(diag_on)
             if N_on == 0:  # Null hypothesis - all 0 cov. matrix
                 hyp_ind += 1
